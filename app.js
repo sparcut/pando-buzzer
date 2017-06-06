ccb2bdd2f01910f95ae552b0b8a1a7d581e192e7
sparcut <stirton123@gmail.com>
2017-06-06 20:48:53 +1000

    port handler fixed

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ io.on('connection', (socket) => {
   });
 });
 
-http.listen(3000, () => {
-  console.log('Listening on port 3000');
+let port = process.env.PORT || 3000;
+http.listen(port, () => {
+  console.log(`Application listening on port ${port}.`);
 });
